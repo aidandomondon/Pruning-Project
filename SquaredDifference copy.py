f4a0564291d236c87ea83a438759020d65133f3e
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2023-12-17 14:32:49 -0500

    Removes squared difference function

--- SquaredDifference copy.py
+++
@@ -1,5 +0,0 @@
-import torch;
-
-# sum of feature map differences for all images in a given batch
-def sum_feature_map_changes(old_featmaps, new_featmaps):
-    return torch.linalg.matrix_norm(old_featmaps - new_featmaps, dim=(2, 3)).sum();
